3630e0e4e7ef5cc9d8bb566b092738ab69a357dc
jtoews32 <jtoews32@gmail.com>
2020-05-17 19:27:29 -0700

    update

--- src/main/java/com/scheduler/config/BeanConfig.java
+++ src/main/java/com/scheduler/config/BeanConfig.java
@@ -3,7 +3,6 @@ package com.scheduler.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.scheduler.modules.*;
@@ -13,7 +12,6 @@ public class BeanConfig {
     private static final Logger log = LoggerFactory.getLogger(BeanConfig.class);
     @Autowired EventRepository eventRepository;
 
-    
     @Bean public EventService eventService() { 
         return new EventServiceImpl(eventRepository);
     }
